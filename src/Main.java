dc5badfe051279622059de3dce960a29f623b8c0
Andrey Lobachev <lobatchev.andrew@gmail.com>
2023-04-06 11:50:53 +0300

    Methods BinaryOperator, UnaryOperator, Predicate, Consumer moved from Class Main to Class Calculator.

--- src/Main.java
+++ src/Main.java
@@ -1,60 +1,31 @@
-import java.util.function.Supplier;
+import java.util.function.*;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator();
 
-//        Calculator calculator = Calculator.supplier.get();
-
-        BinaryOperator<Integer> plus = (x, y) -> x + y;
-        BinaryOperator<Integer> minus = (x, y) -> x - y;
-        BinaryOperator<Integer> multiply = (x, y) -> x * y;
-        BinaryOperator<Integer> devide = (x, y) -> x / y;
-
-        System.out.println(plus.apply(3, 5)); // 8
-        System.out.println(minus.apply(3, 5)); // -2
-        System.out.println(multiply.apply(3, 5)); // 15
-        System.out.println(devide.apply(3, 5)); // 0
-
-        UnaryOperator<Integer> pow = x -> x * x;
-        UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
-
-        System.out.println(pow.apply(2)); //4
-        System.out.println(abs.apply(-5)); //5
-
-        Predicate<Integer> isPositive = x -> x > 0;
-        System.out.println(isPositive.apply(-1)); //false
-
-        Consumer<Integer> println = System.out::println;
-        println.apply(5); // print 5
+        System.out.println(calculator.plus.apply(3, 5)); // 8
+        System.out.println(calculator.minus.apply(3, 5)); // -2
+        System.out.println(calculator.multiply.apply(3, 5)); // 15
+        System.out.println(calculator.devide.apply(3, 5)); // 0
+        System.out.println(calculator.devide.apply(3, 0)); // null
+        System.out.println(calculator.pow.apply(2)); // 4
+        System.out.println(calculator.abs.apply(-5)); // 5
+        System.out.println(calculator.isPositive.test(-1)); // false
+        calculator.println.accept(5); // print 5
     }
 }
 
 class Calculator {
     static Supplier<Integer> supplier;
-
-    public void get(){
-
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> devide = (x, y) -> y != 0 ? x / y : null;
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+    public void get() {
     }
-
-    public Calculator() {
-
-    }
-
-
-}
-
-interface BinaryOperator<Integer> {
-    Integer apply(Integer a, Integer b);
-}
-
-interface UnaryOperator <Integer>{
-    Integer apply (Integer a);
-}
-
-interface Predicate <Integer> {
-    boolean apply (Integer a);
-}
-
-interface  Consumer <Integer> {
-    void apply (Integer a);
 }
